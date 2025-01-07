b0a140a13caa352c0950daa8ceff43f3d267c9d8
minskk <saengki0502@naver.com>
2025-01-07 15:19:37 +0900

    starting the project (2025.01.07) uploading the initial files

---
+++ MyGradeManBasic_python.py
@@ -0,0 +1,8 @@
+#Grade management system
+#
+#ID indicates array index (1,2,..,MAX_STUDENT-1)
+#Global variable : Structure array
+#Max number of students are fixed
+#
+#Written by Minseok Song (2025.01.07)
+
